3ad12fc6857f3c1eedcb6a6f6e5c784a6df1e3e9
Tomas1V10 <vargastomas927@gmail.com>
2023-04-12 21:23:05 -0500

    configuracion base y se agregan los estilos

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,30 @@
-// Sirve para identficar la ruta de donde se encuentra este archivo
-const path = requiere('path');
+// Sirve para identificar la ruta de donde se encuentra este archivo
+const path = require('path');
 
 // Me permite trabajar con documentos html
-const HtmlWebpackPLugin = requiere('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// Extraer el codigo css, minificarlo y optimizarlo. ademas lo agrega como parte del head
-const MiniCssExtractPlugin = requiere('mini-css-extract-plugin');
+// Extraer el código css, minificarlo y optimizarlo. Ademas lo agrega como parte del head
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-// Nos permite copiar archivos de uan ruta a otra
-const CopyWebpackPlugin = requiere('copy-webpack-plugin');
+// Nos permite copiar archivos de una ruta a otra
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
-    // operadores en javascript, que diferencia existe entre el operador == y el ===
     const isProduction = argv.mode === 'production';
-
     return {
         entry: {
-            index:  './src/index.js',
+            index: './src/index.js',
+            styles: './src/styles.js'
         },
         output: {
-            filename: '[name].js',
+            filename: '[name].[contenthash].js',
             path: path.resolve(__dirname, 'dist')
         },
         module: {
             rules: [
-
                 {
-                    test: /\css$/,
+                    test: /\.css$/,
                     use: [
                         isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
                         'css-loader'
@@ -37,24 +35,30 @@ module.exports = (env, argv) => {
                     include: path.resolve(__dirname, 'src/assets/js'),
                     use: {
                         loader: 'babel-loader',
-                        options:{
+                        options: {
                             presets: ['@babel/preset-env']
                         }
                     }
                 }
             ]
         },
-        Plugins: [],
-        devserver: {
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                chunks: ['index', 'styles']
+            }),
+            // averiguar que significa un spread operator
+            ...(isProduction ? [new MiniCssExtractPlugin({ filename: 'assets/css/[name].[contenthash].css' })] : [])
+        ],
+        devServer: {
             static: {
                 directory: path.join(__dirname, 'dist'),
             },
             open: true,
             hot: true,
-            warchFiles: [
+            watchFiles: [
                 'src/**/*'
             ]
         }
     };
-
 }
